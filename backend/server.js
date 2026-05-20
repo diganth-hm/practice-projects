9c2523071e78c14629571828309be0eee3c28b80
diganth-hm <diganthhm714@gmail.com>
2026-05-21 00:49:50 +0530

    debuged and correctd the syntax

--- backend/server.js
+++ backend/server.js
@@ -5,18 +5,45 @@ app.use(express.json());
 
 app.post("/signup",(req,res)=>
 {
- const [email,pass]=req.body;
+ const {email,pass}=req.body;
  if (email=="" || pass == "")
  {
-  return   res.status(400);
-    console.log("bad request")
+        console.log("bad request")
+
+    return   res.status(400).send("Fields cannot be empty");
 
  }
  
-console.log("server created");
+return res.status(201).send("Signup successful");
 
 });
-server.listen(5000,()=>
+
+
+//Login page
+app.post("/login",(req,res)=>
+{
+    const{email,pass}=req.body;
+    if(email == "" || pass == "")
+    {
+       console.log("bad request");
+        return res.status(400).send("Fields can not be empty");
+        
+    }
+    if(email == "diagnthhm714@gmail.com" && pass == "12345678")
+    {
+    console.log("User  registered sucessfully")
+    return res.status(201).send("Login sucessfull");
+   
+    }
+    if(email !="diagnthhm714@gmail.com" && pass !="12345678")
+    {
+         console.log("bad reaquest");
+        return res.status(401).send("invalid eamil or password");
+       
+    }
+});
+
+app.listen(5000,()=>
 {
     console.log("server running on port 5000");
 });
